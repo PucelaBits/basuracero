36db3c433498c15692c374ce8a7664c4ab94f44a
nukeador <nukeador@protonmail.com>
2024-09-21 19:00:58 +0200

    sin routers

--- src/client/main.js
+++ src/client/main.js
@@ -1,21 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import ListaIncidencias from './components/ListaIncidencias.vue'
-import DetalleIncidencia from './components/DetalleIncidencia.vue'
 import './assets/main.css'
 import 'leaflet/dist/leaflet.css'
 
-const routes = [
-  { path: '/', component: ListaIncidencias },
-  { path: '/i/:id', component: DetalleIncidencia, props: true }
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
