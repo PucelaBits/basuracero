2e09cf7f0b63301b3c8921b59f119b467cee2427
nukeador <nukeador@protonmail.com>
2024-09-21 18:42:46 +0200

    Package name

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import ListaIncidencias from './components/ListaIncidencias.vue'
+import DetalleIncidencia from './components/DetalleIncidencia.vue'
 import './assets/main.css'
 import 'leaflet/dist/leaflet.css'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: ListaIncidencias },
+  { path: '/i/:id', component: DetalleIncidencia, props: true }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
