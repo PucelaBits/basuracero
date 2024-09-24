76c020afb752a8c19c09f0daf510040c60c0a183
nukeador <nukeador@protonmail.com>
2024-09-24 21:11:47 +0200

    Direcciones en español

--- src/client/components/ReportarIncidencia.vue
+++ src/client/components/ReportarIncidencia.vue
@@ -213,7 +213,7 @@ export default {
       if (incidencia.value.latitud && incidencia.value.longitud) {
         const validacionResult = validarCoordenadas();
         if (validacionResult === true) {
-          const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${incidencia.value.latitud}&lon=${incidencia.value.longitud}&zoom=18&addressdetails=1`
+          const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${incidencia.value.latitud}&lon=${incidencia.value.longitud}&zoom=18&addressdetails=1&accept-language=es`
           try {
             const response = await fetch(url)
             const data = await response.json()
