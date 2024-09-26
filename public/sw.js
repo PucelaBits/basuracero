556f1d84fc099f5b16ac7de5018d53249b366361
nukeador <nukeador@protonmail.com>
2024-09-26 13:21:51 +0200

    sin cache en pwa

--- public/sw.js
+++ public/sw.js
@@ -1,40 +1,13 @@
-const CACHE_NAME = 'basura-cero-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.png'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(Promise.resolve());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    fetch(event.request)
   );
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(Promise.resolve());
 });
