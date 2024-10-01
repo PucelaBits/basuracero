fb9845df2ca0911fe7cb9545b9a1657135159d68
nukeador <nukeador@protonmail.com>
2024-10-01 20:11:34 +0200

    revert

--- src/server/server.js
+++ src/server/server.js
@@ -19,62 +19,6 @@ app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
 app.use('/api/incidencias', incidenciasRoutes);
 app.use('/api/rss', rssRoutes);
 
-// Manejar rutas de incidencias individuales
-app.get('/incidencia/:id', async (req, res) => {
-  const id = req.params.id;
-  try {
-    const incidencia = await new Promise((resolve, reject) => {
-      db.get('SELECT * FROM incidencias WHERE id = ?', [id], (err, row) => {
-        if (err) reject(err);
-        else resolve(row);
-      });
-    });
-
-    if (!incidencia) {
-      return res.status(404).send('Incidencia no encontrada');
-    }
-
-    let indexHtml = await fs.promises.readFile(path.join(__dirname, '..', '..', 'dist', 'index.html'), 'utf8');
-
-    const title = `Basura Cero - Incidencia ${incidencia.id}`;
-    const description = incidencia.descripcion || 'Detalles de la incidencia en Basura Cero';
-    const imageUrl = incidencia.imagenes && incidencia.imagenes.length > 0 
-      ? `https://basuracero.pucelabits.org/uploads/${incidencia.imagenes[0].ruta_imagen}` 
-      : 'https://basuracero.pucelabits.org/favicon.png';
-    const url = `https://basuracero.pucelabits.org/incidencia/${incidencia.id}`;
-
-    // Actualizar los metadatos
-    const metadataUpdates = [
-      { tag: 'title', content: title },
-      { property: 'og:title', content: title },
-      { property: 'og:description', content: description },
-      { property: 'og:image', content: imageUrl },
-      { property: 'og:url', content: url },
-      { name: 'twitter:title', content: title },
-      { name: 'twitter:description', content: description },
-      { name: 'twitter:image', content: imageUrl },
-      { name: 'twitter:url', content: url },
-    ];
-
-    metadataUpdates.forEach(update => {
-      const regex = update.tag 
-        ? new RegExp(`<${update.tag}>.*?</${update.tag}>`)
-        : new RegExp(`<meta (property|name)="${update.property || update.name}".*?>`);
-      
-      const replacement = update.tag
-        ? `<${update.tag}>${update.content}</${update.tag}>`
-        : `<meta ${update.property ? 'property' : 'name'}="${update.property || update.name}" content="${update.content}">`;
-      
-      indexHtml = indexHtml.replace(regex, replacement);
-    });
-
-    res.send(indexHtml);
-  } catch (error) {
-    console.error('Error al obtener la incidencia:', error);
-    res.status(500).send('Error interno del servidor');
-  }
-});
-
 // Manejar todas las demás rutas y servir index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
