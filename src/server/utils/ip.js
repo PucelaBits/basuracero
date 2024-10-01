c7cc93b49c9a9f76108f2a210a24313014b54c3f
nukeador <nukeador@protonmail.com>
2024-10-01 22:55:31 +0200

    Fuera logs de debug temporales

--- src/server/utils/ip.js
+++ src/server/utils/ip.js
@@ -1,19 +1,12 @@
 const crypto = require('crypto');
 
 function obtenerIP(req) {
-  console.log('Headers:', req.headers);
-  console.log('X-Forwarded-For:', req.headers['x-forwarded-for']);
-  console.log('X-Real-IP:', req.headers['x-real-ip']);
-  console.log('Connection remote address:', req.connection.remoteAddress);
-  
   const ip = req.headers['x-forwarded-for']?.split(',')[0].trim() || 
              req.headers['x-real-ip'] || 
              req.connection.remoteAddress ||
              req.socket.remoteAddress ||
              req.connection.socket?.remoteAddress ||
              'Desconocida';
-  
-  console.log('IP obtenida:', ip);
   return ip === '::1' || ip === '127.0.0.1' ? 'Desconocida' : hashearIP(ip);
 }
 
