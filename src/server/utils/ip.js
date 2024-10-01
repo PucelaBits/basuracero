a9d07e0eeb532cb34eb765e3bdd34dd212fe67f5
nukeador <nukeador@protonmail.com>
2024-10-01 22:40:34 +0200

    Debug temporal

--- src/server/utils/ip.js
+++ src/server/utils/ip.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
 
 function obtenerIP(req) {
+  console.log('Headers:', req.headers);
+  console.log('Connection remote address:', req.connection.remoteAddress);
   const ip = req.headers['x-real-ip'] || 
              req.headers['x-forwarded-for']?.split(',')[0] || 
              req.connection.remoteAddress ||
